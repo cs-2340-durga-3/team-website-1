7aebb6469cc3705b73923916a82af39f5e99580e
cubedhuang <99danielh@gmail.com>
2025-02-20 14:28:28 -0500

    use youtube embed instead

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,7 +1,13 @@
 <div class="py-16">
 	<h1 class="text-4xl font-semibold text-white">Video Demonstration</h1>
 
-	<video class="mt-8 w-full rounded-xl" controls src="/project/demo.mov">
-		<track kind="captions" />
-	</video>
+	<iframe
+		src="https://www.youtube.com/embed/lro6d6dNZ0w"
+		title="YouTube video player"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+		referrerpolicy="strict-origin-when-cross-origin"
+		allowfullscreen
+		class="mt-8 aspect-video w-full"
+	></iframe>
 </div>
